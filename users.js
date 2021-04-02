aa93ac686f8aff14acd3cd7f9426af8d778c3350
teresaa98 <70562048+teresaa98@users.noreply.github.com>
2021-04-01 22:47:09 -0700

    Add files via upload
    finished step 5 from https://closebrace.com/tutorials/2017-03-02/creating-a-simple-restful-web-app-with-nodejs-express-and-mongodb tutorial

---
+++ users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
+});
+
+module.exports = router;
