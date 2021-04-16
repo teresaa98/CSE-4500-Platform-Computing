81baae14b0925e86df0bac271ad5a3c5df8678c9
teresaa98 <70562048+teresaa98@users.noreply.github.com>
2021-04-15 23:26:41 -0700

    Update users.js
    updated final version of user.js

--- users.js
+++ users.js
@@ -10,4 +10,23 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
+});
+
+router.delete('/deleteuser/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+  });
+});
+
 module.exports = router;
